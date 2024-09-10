c466d252b2cbf318a57f22b0a91d94d0d0bf6abe
LYH <dudgk7829@naver.com>
2024-09-10 10:31:04 +0900

    20240910

---
+++ src/main/java/com/korit/senicare/SenicareApplication.java
@@ -0,0 +1,13 @@
+package com.korit.senicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenicareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korit/senicare/SenicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.senicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
